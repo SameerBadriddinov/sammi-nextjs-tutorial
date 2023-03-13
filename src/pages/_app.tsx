81a40a7f0352989a28a02ee59bd44d10097f7c1a
Samar <samarbadriddinov.tv@gmail.com>
2023-03-13 06:53:51 +0200

    NPRogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import 'src/styles/globals.css';
+import 'nprogress/nprogress.css';
+
 import type { AppProps } from 'next/app';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import createEmotionCache from 'src/helpers/create-emotion-cashe';
@@ -6,6 +8,9 @@ import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from 'src/helpers/theme';
 import { CssBaseline } from '@mui/material';
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
+import Router from 'next/router';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -15,6 +20,21 @@ export interface MyAppProps extends AppProps {
 
 function MyApp(props: MyAppProps) {
 	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+	useEffect(() => {
+		const handleRouteStart = () => NProgress.start();
+		const handleRouteDone = () => NProgress.done();
+
+		Router.events.on('routeChangeStart', handleRouteStart);
+		Router.events.on('routeChangeComplete', handleRouteDone);
+		Router.events.on('routeChangeError', handleRouteDone);
+
+		return () => {
+			Router.events.off('routeChangeStart', handleRouteStart);
+			Router.events.off('routeChangeComplete', handleRouteDone);
+			Router.events.off('routeChangeError', handleRouteDone);
+		};
+	}, []);
 
 	return (
 		<CacheProvider value={emotionCache}>
